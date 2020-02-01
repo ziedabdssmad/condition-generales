1928b84930810aee1a433277dd1d0bc9c51aed61
unknown <ziedabdssmad123@gmail.com>
2020-02-01 15:24:35 +0100

    yallaa

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     // res.render('index', { title: 'Express' });
     // res.sendFile('./../views/condition-general/index.html');
-    res.sendFile('index.html', { root: path.join(__dirname, './../views/condition-general') });
+    res.sendFile(path.resolve(app.get('./../views/condition-general') + '/index.html'));
+
+    // res.sendFile('index.html', { root: path.join(__dirname, './../views/condition-general') });
 
     // res.sendFile(path.resolve(__dirname, 'views', 'dist','Mreyet0', 'index.html'))
 });
