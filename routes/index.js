895598dfa38f4dce473134297fc039a12cc9ace6
unknown <ziedabdssmad123@gmail.com>
2020-02-01 14:07:21 +0100

    go

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    // res.render('index', { title: 'Express' });
+    res.sendFile('./../views/condition-general/index.html');
+    // res.sendFile(path.resolve(__dirname, 'views', 'dist','Mreyet0', 'index.html'))
+});
+
+module.exports = router;
